6952a2bd18a190e0a471736026702431d47d79d3
tarkIlJun <tack89826@gmail.com>
2023-06-29 22:02:14 +0900

    test3

--- src/components/Kiosk.js
+++ src/components/Kiosk.js
@@ -8,6 +8,23 @@ const products = [
 
 ]
 
+const getTotal = (arr) => {
+
+    if(!arr || arr.length === 0){
+        return 0
+    }
+
+    let sum = 0;
+    // reduce
+    for(const ele of arr ){
+        sum += (ele.price * ele.qty)
+    }
+
+    return sum
+
+}
+
+
 const Kiosk = () => {
 
     const [items, setItems] = useState([])
@@ -30,21 +47,46 @@ const Kiosk = () => {
 
         
     }
+
+
+    const handleClickQty = (pno , amount) => {
+
+        console.log("pno" , pno , "amount" , amount)
+        const target = items.filter( item => item.pno === pno )[0]
+
+        console.log(target)
+
+
+        if(amount === 1){
+            target.qty += 1
+            setItems([...items])
+        }else {
+            if(target.qty === 1){
+                setItems( items.filter(ele => ele.pno !== pno) )
+                return
+            }
+            target.qty -= 1
+            setItems([...items])
+        }
+
+
+
+    }
     
     
     
     return ( 
 
-        <div className="w-full h-[100vh] bg-blue-600 flex"> 
+        <div className="w-full h-[100vh] bg-blue-500 flex"> 
             <div className="w-2/3 bg-white ">
                 <div className="text-4xl font-extrabold">Products</div>
                 
                 <ul>
                     {products.map( p => 
                     <li
-                    key={p.pno} 
-                    className="text-2xl underline m-3 p-3 bg-red-600"
-                    onClick={() => {handleClickBuy(p)}}
+                        key={p.pno} 
+                        className="text-2xl underline m-3 p-3 bg-red-600"
+                        onClick={() => {handleClickBuy(p)}}
                     >
                         {p.pno} - {p.pname} - {p.price}
                     <button className="border-2 m-2 p-2 rounded-lg border-y-pink-400">BUY</button>
@@ -59,18 +101,33 @@ const Kiosk = () => {
             <ul>
                 {items.map( (item, idx) => 
                 <li key={idx} className="border-2">
-                    <div className="flex text-3xl text-black m-4 p-4">
+                    <div className="flex text-3xl text-white m-4 p-4">
                         <div>{item.pno}</div>
                         <div>{item.pname}</div>
                         <div>{item.price}</div>
                     </div>
                     <div className="flex justify-center text-2xl">
-                        <button className="rounded-lg bg-orange-300 p-4 m-1">+</button>
-                        <p className="m-2 text-red-700 p-2">{item.qty}</p>
-                        <button className="rounded-lg bg-orange-300 p-4 m-1">-</button>
+                        <button 
+                            className="rounded-lg bg-orange-300 p-4 m-1"
+                            onClick={() => handleClickQty(item.pno, 1)}
+                        
+                        >
+                            +
+                        </button>
+                        <p className="m-2 text-red-300 p-2">{item.qty}</p>
+                        <button 
+                            className="rounded-lg bg-orange-300 p-4 m-1"
+                            onClick={() => handleClickQty(item.pno, -1)}
+                        >
+                            -
+                        </button>
                     </div>
                 </li> )}
             </ul>
+
+            <div className="bg-blue-300 text-5xl ">
+                Total {getTotal(items)}
+            </div>
 
             </div>
         </div>
